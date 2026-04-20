6b49ab5d3bd85dfab192ef7af5aa84ea4d3da543
rizinaa99 <66684341+rizinaa99@users.noreply.github.com>
2026-04-20 14:23:52 +0100

    PSEC-2920 fix wildcard matching

--- policyexplorer/common.py
+++ policyexplorer/common.py
@@ -11,7 +11,9 @@ def ensure_array(element: str | List[str]) -> List[str]:
 
 def pattern_to_regex(wildcard: str) -> str:
     # TODO: need to handle "?" too esp when there are more than 1 "?"
-    return ".*".join(wildcard.split("*")) + "$"
+    escaped = re.escape(wildcard)
+    escaped = escaped.replace(r"\*", ".*")
+    return f"^{escaped}$"
 
 
 def matches_pattern(pattern: str, string: str) -> bool:
